38fc232e392de3912f7e50ebd97df574134e2c7e
mfarhann10 <mfarhann1009@gmail.com>
2024-09-29 16:27:46 +0700

    add table secret

--- index.js
+++ index.js
@@ -58,9 +58,35 @@ app.get("/logout", (req, res) =>{
   });
 });
 
-app.get("/secrets", (req, res) => {
+app.get("/secrets", async (req, res) => {
+  console.log(req.user);
+
+  ////////////////UPDATED GET SECRETS ROUTE/////////////////
   if (req.isAuthenticated()) {
-    res.render("secrets.ejs", { name: req.user.name }); // Pass 'name' to secrets.ejs
+    try {
+      const result = await db.query(
+        `SELECT secret FROM users WHERE email = $1`,
+        [req.user.email]
+      );
+      console.log(result);
+      const secret = result.rows[0].secret;
+      if (secret) {
+        res.render("secrets.ejs", { secret: secret , name: req.user.name});
+      } else {
+        res.render("secrets.ejs", { secret: "Jack Bauer is my hero." });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+////////////////SUBMIT GET ROUTE/////////////////
+app.get("/submit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit.ejs");
   } else {
     res.redirect("/login");
   }
@@ -117,6 +143,21 @@ app.post("/register", async (req, res) => {
   }
   catch(err){
     console.log(err)
+  }
+});
+
+////////////////SUBMIT POST ROUTE/////////////////
+app.post("/submit", async function (req, res) {
+  const submittedSecret = req.body.secret;
+  console.log(req.user);
+  try {
+    await db.query(`UPDATE users SET secret = $1 WHERE email = $2`, [
+      submittedSecret,
+      req.user.email,
+    ]);
+    res.redirect("/secrets");
+  } catch (err) {
+    console.log(err);
   }
 });
 
